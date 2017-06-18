6be7eeac9f51fb675425a44e1f25c6a476eeb9b6
JackPeng <jackpeng1203@gmail.com>
2017-06-18 17:44:06 +0000

    fifth push

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,13 +1,35 @@
 class TodoController < ApplicationController
     def index
     end
+    def settings
+    end
+    def filter
+    end
     def show
+    
         todo_id = params[:id]
-        @todo_description = "ggi"
         if todo_id == '1'
-            @todo_description = "Task 1"
+            @todo_description = "Do homework"
         elsif todo_id == '2'
-            @todo_description = "Task 2"
+            @todo_description = "Excercise"
+            elsif todo_id == '3'
+            @todo_description = "Do project"
+            elsif todo_id == '4'
+            @todo_description = "Sign up for volunteering"
+            elsif todo_id == '5'
+            @todo_description = "Complete drivers test"
+            elsif todo_id == '6'
+            @todo_description = "Do SATs"
+            elsif todo_id == '7'
+            @todo_description = "Practice basketball"
+            elsif todo_id == '8'
+            @todo_description = "Do chores"
+            elsif todo_id == '9'
+            @todo_description = "Walk dog"
+            elsif todo_id == '10'
+            @todo_description = "Feed dog"
+        else
+            @todo_description = "Task 0"
         end
         
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'todo/index',to: 'todo#index'
   get 'todo/show/:id',to: 'todo#show'
+  get 'todo/settings/',to: 'todo#settings'
+  get 'todo/filter/',to: 'todo#filter'
 end
